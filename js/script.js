b10e7af14d3887345963a406ce726cc1c6fb96db
Jey2503KM <102761750+Jey2503KM@users.noreply.github.com>
2023-11-03 22:23:21 -0500

    dfsfsf

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const navbar = document.querySelector(".navbar");
-    const navbarHeight = navbar.clientHeight; 
-    let isNavbarFixed = false;
+  const navbar = document.querySelector(".navbar");
+  const navbarHeight = navbar.clientHeight;
+  const buttons = document.querySelectorAll(".movie-filter button");
+  const cards = document.querySelectorAll(".col");
+  let isNavbarFixed = false;
 
-   
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > navbarHeight) {
-            if (!isNavbarFixed) {
-                navbar.classList.add("navbar-fixed");
-                isNavbarFixed = true;
-            }
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const category = button.getAttribute("title");
+
+      cards.forEach((card) => {
+        const cardCategory = card.getAttribute("data-category");
+
+        if (category === "popular" || cardCategory === category) {
+          card.style.display = "block";
         } else {
-            if (isNavbarFixed) {
-                navbar.classList.remove("navbar-fixed");
-                isNavbarFixed = false;
-            }
+          card.style.display = "none";
         }
+      });
     });
+  });
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > navbarHeight) {
+      if (!isNavbarFixed) {
+        navbar.classList.add("navbar-fixed");
+        isNavbarFixed = true;
+      }
+    } else {
+      if (isNavbarFixed) {
+        navbar.classList.remove("navbar-fixed");
+        isNavbarFixed = false;
+      }
+    }
+  });
 
 });
